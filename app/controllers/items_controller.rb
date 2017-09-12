5e00827bcdef7f53cbcd24fdc5d83ad655b34d24
Joshua Anderson <doomdhitman@gmail.com>
2017-09-12 08:58:23 +0000

    WIP create items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,29 +1,9 @@
 class ItemsController < ApplicationController
-  def index
-    @items = Item.all
-  end
-
-  def show
-    
-  end
-
-  def new
-  end
-
-  def edit
-    
-  end
+  before_action :require_sign_in
   
   def create
-    @item = Item.new
-    @item.name = params[:item][:name]
-    @item.user = params[:post][:user]
-    if @item.save
-      flash[:notice] = "Item was saved successfully."
-      redirect_to @item
-    else
-      flash.now[:alert] = "There was an error saving the post. Please try again later."
-      render :new
-    end
   end
+
+      
+
 end
