3ab97a32e49afdd2d7098cb3d014589bcd3ca573
Joshua Anderson <doomdhitman@gmail.com>
2017-09-13 00:15:00 +0000

    corrected items controller actions, and WIP for items in users#show view.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,21 @@ class ItemsController < ApplicationController
   before_action :require_sign_in
   
   def create
+    @item = Item.new
+    @item.name = params[:item][:name]
+    @item.user = params[:item][:user]
+    
+    if @item.save
+      
+      flash[:notice] = "Item was saved successfully."
+      redirect_to @user
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :create
+    end
+  end
+  
+  def delete
   end
 
       
