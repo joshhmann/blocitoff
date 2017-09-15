0a5935c1c836fe44d40ef45dcbb4bf547b0b5ca0
Joshua Anderson <doomdhitman@gmail.com>
2017-09-15 22:39:26 +0000

    fixed routes that led to dead end links when loading up website or logging in

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   get 'about' => 'welcome#about'
-  root 'user#show'
+  root 'users#show'
   resources :items, only: [:create]
 
   
